659cee3ec5d33e68a9db83198e157f74d89c87d0
Álvaro Emparán <alvaro.emparan@gmail.com>
2020-04-10 17:22:43 -0400

    update

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -37,6 +37,7 @@ namespace WebApi.Controllers
             return Ok(users);
         }
 
+        [Authorize(Roles = Role.User)]
         [HttpGet("{id}")]
         public IActionResult GetById(int id)
         {
